4ac57dd402b44ecdcc5bae726866be9f2fe91d22
Mike Holzbach <michael.holzbach@elinvar.de>
2024-01-02 09:40:37 +0100

    Add loader skeleton

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,18 @@
+import "./loading.scss";
+
 export default function Loading() {
-  return <p>Loading...</p>;
+  return (
+    <div className="skeleton">
+      <div className="skeleton__animation skeleton__header"></div>
+      <hr />
+      <div className="skeleton__animation skeleton__text"></div>
+      <div className="skeleton__animation skeleton__text"></div>
+      <div className="skeleton__buttons">
+        <div className="skeleton__animation skeleton__button"></div>
+        <div className="skeleton__animation skeleton__button"></div>
+        <div className="skeleton__animation skeleton__button"></div>
+      </div>
+      <div className="skeleton__animation skeleton__images"></div>
+    </div>
+  );
 }
